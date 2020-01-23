4e30137aefe86638ce32e881a1b42c9ecd752ce1
ShabanKamell <sh3ban.kamel@gmail.com>
2020-01-23 16:29:03 +0200

    ignore redundant files

--- common/data/src/main/java/currencyconverter/common/data/network/api/CurrencyRateApi.kt
+++ common/data/src/main/java/currencyconverter/common/data/network/api/CurrencyRateApi.kt
@@ -1,6 +1,5 @@
 package currencyconverter.common.data.network.api
 
-import currencyconverter.common.data.BuildConfig
 import currencyconverter.common.data.model.CurrencyRateResponse
 import io.reactivex.Single
 import retrofit2.http.GET
@@ -10,6 +9,6 @@ interface CurrencyRateApi {
     @GET("latest")
     fun latestRates(
             @Query("country") country: String = "US",
-            @Query("access_key") accessKey: String = BuildConfig.FIXER_ACCESS_KEY
+            @Query("access_key") accessKey: String = FixerAccessKey.KEY
     ): Single<CurrencyRateResponse>
 }
---
+++ common/data/src/main/java/currencyconverter/common/data/network/api/FixerAccessKey.kt
@@ -0,0 +1,5 @@
+package currencyconverter.common.data.network.api
+
+object FixerAccessKey {
+    const val KEY = "818db86f8083d887073a235d2c0ed6c0"
+}
