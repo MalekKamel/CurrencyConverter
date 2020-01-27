b2a11a1307218d87cd83636b7bff3a76a5a05ef7
ShabanKamell <sh3ban.kamel@gmail.com>
2020-01-27 10:57:25 +0200

    abstract SharedPref in AbsSharedPref

--- common/data/src/main/java/currencyconverter/common/data/pref/SharedPref.kt
+++ common/data/src/main/java/currencyconverter/common/data/pref/SharedPref.kt
@@ -6,52 +6,67 @@ import android.content.SharedPreferences
  * Created by Sha on 4/20/17.
  */
 
-class SharedPref(private val pref: SharedPreferences) {
+class SharedPref(pref: SharedPreferences): AbsSharedPref(pref) {
+    var token: String
+        get() = getString(SPKey.TOKEN, "")
+        set(value) {
+            putString(value, SPKey.TOKEN)
+        }
 
+    var refreshToken: String
+        get() = getString(SPKey.REFRESH_TOKEN, "")
+        set(value) {
+            putString(value, SPKey.REFRESH_TOKEN)
+        }
+
+    fun isLogged(): Boolean = token.isNotEmpty()
+}
+
+abstract class AbsSharedPref(private val pref: SharedPreferences) {
     private var editor: SharedPreferences.Editor = pref.edit()
 
-    private fun putString(value: String?, key: Key){
+    fun putString(value: String?, key: SPKey){
         editor.putString(key.name, value)
         editor.apply()
     }
 
-    private fun putInt(value: Int, key: Key){
+    fun putInt(value: Int, key: SPKey){
         editor.putInt(key.name, value)
         editor.apply()
     }
 
-    private fun putLong(value: Long, key: Key){
+    fun putLong(value: Long, key: SPKey){
         editor.putLong(key.name, value)
         editor.apply()
     }
 
-    private fun putFloat(value: Float, key: Key){
+    fun putFloat(value: Float, key: SPKey){
         editor.putFloat(key.name, value)
         editor.apply()
     }
 
-    private fun putBoolean(value: Boolean, key: Key){
+    fun putBoolean(value: Boolean, key: SPKey){
         editor.putBoolean(key.name, value)
         editor.apply()
     }
 
-    private fun getString(key: Key, def: String): String{
+    fun getString(key: SPKey, def: String): String{
         return pref.getString(key.name, def)
     }
 
-    private fun getInt(key: Key, def: Int): Int{
+    fun getInt(key: SPKey, def: Int): Int{
         return pref.getInt(key.name, def)
     }
 
-    private fun getLong(key: Key, def: Long): Long{
+    fun getLong(key: SPKey, def: Long): Long{
         return pref.getLong(key.name, def)
     }
 
-    private fun getFloat(key: Key, def: Float): Float{
+    fun getFloat(key: SPKey, def: Float): Float{
         return pref.getFloat(key.name, def)
     }
 
-    private fun getBoolean(key: Key, def: Boolean): Boolean{
+    fun getBoolean(key: SPKey, def: Boolean): Boolean{
         return pref.getBoolean(key.name, def)
     }
 
@@ -59,29 +74,10 @@ class SharedPref(private val pref: SharedPreferences) {
         editor.clear()
         editor.apply()
     }
-
-    ///// Impl
-
-    private enum class Key{
-        TOKEN,
-        REFRESH_TOKEN
-    }
-
-    var token: String
-        get() = getString(Key.TOKEN, "")
-        set(value) {
-            putString(value, Key.TOKEN)
-        }
-
-    var refreshToken: String
-        get() = getString(Key.REFRESH_TOKEN, "")
-        set(value) {
-            putString(value, Key.REFRESH_TOKEN)
-        }
-
-    fun isLogged(): Boolean = token.isNotEmpty()
-
-
-
-
 }
+
+enum class SPKey {
+    TOKEN,
+    REFRESH_TOKEN
+}
+
